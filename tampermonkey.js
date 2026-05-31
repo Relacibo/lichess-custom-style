e37396754d1c1fad0a2ef9d967fdaf4acb95981b
Reinhard Bronner <reinhard_bronner@t-online.de>
2026-05-31 15:55:50 +0200

    version

--- tampermonkey.js
+++ tampermonkey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Relacibos Lichess userscript
 // @namespace    Tampermonkey Scripts
-// @version      0.31
+// @version      0.32
 // @license MIT
 // @description  My custom lichess UX/UI enhancements
 // @author       Relacibo
